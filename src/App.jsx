3bc02a8dc61520a5094573af3d77eaae82371c0a
Ahmed Tiger <ahmeddhaqan11@gmail.com>
2026-04-14 16:39:59 +0300

    LESSON 4 FLEX and GRID

--- src/App.jsx
+++ src/App.jsx
@@ -4,51 +4,17 @@ function App() {
 
   return (
     <div>
-      {/* Colors */}
-      {/* <h1 className='bg-red-500 text-white'>hellow CA2026</h1>
-      <h1 className='bg-amber-700 text-white'>hellow CA2026</h1>
-      <div className='container'>
-        <h2>Welcome</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias excepturi tempore magni. A, magnam. Praesentium exercitationem hic rem molestias minus illum harum voluptatum optio doloremque. Necessitatibus excepturi et inventore tenetur.</p>
-      </div>
-
-      <div className='bg-cyan-400 m-3'>Lorem ipsum dolor sit amet</div>
-      <div className='bg-cyan-800 mt-3'>Lorem ipsum dolor sit amet</div>
-      <div className='bg-cyan-500 mx-3 mt-3'>Lorem ipsum dolor sit amet</div>
-      <div className='bg-cyan-900  my-3'>Lorem ipsum dolor sit amet</div>
-      <div className='bg-cyan-500  mb-3'>Lorem ipsum dolor sit amet</div>
-
-      <div className='bg-blue-400 p-3'>Lorem ipsum dolor sit amet</div>
-      <div className='bg-blue-800 pt-3'>Lorem ipsum dolor sit amet</div>
-      <div className='bg-blue-500 pb-3'>Lorem ipsum dolor sit amet</div>
-      <div className='bg-blue-900  py-3'>Lorem ipsum dolor sit amet</div>
-      <div className='bg-blue-500  pb-3'>Lorem ipsum dolor sit amet</div>
-
-
-      <div className='text-normal'>WELCOME TO CA234</div>
-      <div className='text-base'>WELCOME TO CA234</div>
-      <div className='text-lg'>WELCOME TO CA234</div>
-      <div className='text-xl'>WELCOME TO CA234</div>
-      <div className='text-2xl underline text-amber-300 decoration-amber-600'>WELCOME TO CA234</div>
-      <div className='text-9xl font-extrabold'>WELCOME TO CA234</div> */}
-
-      {/* <div className='bg-red-500 mx-4 mt-4 p-4 rounded-xl te
-      xt-white'>HELLOW CA234</div> */}
-
-
-      <div className='flex space-x-4'>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-        <div>Item 5</div>
-      </div>
-      <div className='space-y-5'>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
-        <div>Item 5</div>
+      
+      <div className="grid grid-cols-4 gap-2 m-10">
+       <div className="col-span-2 row-span-2 p-10 border border-blue-500 bg-blue-300">items 1</div>
+       <div className="p-10 border border-blue-500 bg-blue-300">items 2</div>
+       <div className="p-10 border border-blue-500 bg-blue-300">items 3</div>
+       <div className="p-10 border border-blue-500 bg-blue-300">items 4</div>
+      <div className="p-10 border border-blue-500 bg-blue-300">items 5</div>
+       <div className="p-10 border border-blue-500 bg-blue-300">items 6</div>
+       <div className="p-10 border border-blue-500 bg-blue-300">items 7</div>
+       <div className="col-span-2 p-10 border border-blue-500 bg-blue-300">items 8</div>
+        
       </div>
 
     </div>
