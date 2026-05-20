60fa083a638348c9002b0f5dfb5db74300115444
Ahmed Tiger <ahmeddhaqan11@gmail.com>
2026-05-20 16:28:22 +0300

    LESSON useContext and useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,35 @@
-import { useState } from 'react';
-import './style.css';
+import React, { useState, useRef } from 'react'
+
 function App() {
-  // let name = "AHMED"
-  const [name, setName] = useState("ALI")
-  const [age, setAge] = useState(20)
-  const [counter, setCounter] = useState(0)
-  const handleClick = (name) => {
-    console.log("HELLOW " + name);
+  console.log("App Component Rendered");
+  const [counter,setCounter]=useState(0);
+  let refCounter=useRef(0);
+  const inputRef=useRef(null);
+
+
+  const handleCounterIncrement=()=>{
+    setCounter(counter+1);
+    
   }
 
-  const handleName = () => {
-    // name="MARYAMA";
-    setName("MARYAMA")
-    setAge(30)
-
-    console.log(name)
+  const handleRefCounterIncrement=()=>{
+    refCounter.current+=1;
+    console.log("Ref Counter",refCounter.current);
   }
 
-  const handleCounter = (isIncrement) => {
-    if (isIncrement) {
-      setCounter(counter + 1)
-    } else {
-      if(counter>0){
-        setCounter(counter - 1)
-      }
-      
-    }
+  const handleInputFocus=()=>{
+    inputRef.current.focus();
   }
-
   return (
-    <div className='m-20'>
-
-      {/* <h2>{name} is {age} years old</h2> */}
-      <h2>COUNTER: {counter}</h2>
-      {/* <button className='p-2 bg-blue-500 text-white rounded hover:bg-blue-600' onClick={handleName}
-      >Click Me</button> */}
-
-      <button className='p-2 bg-blue-500 text-white rounded hover:bg-blue-600' onClick={() => handleCounter(true)}
-      >INCREMEANT</button>
-
-
-      <button className='p-2 bg-red-500 text-white rounded hover:bg-red-600' onClick={() => handleCounter(false)}
-      >DECREMEANT</button>
-
-
+    <div className='flex flex-col items-center justify-center h-screen'>
+      {/* <h1 className='text-2xl font-bold'>Counter: {counter}</h1>
+      <h1 className='text-2xl font-bold'>Ref Counter: {refCounter.current}</h1>
+      <button className='bg-blue-500 text-white p-2 rounded-md' onClick={handleCounterIncrement}>Increment</button> */}
+      <input className='border-2 border-gray-300 p-2 rounded-md' type="text" ref={inputRef}
+      placeholder='Enter your name'
+      />
+      <button className='bg-green-500 mt-4 text-white p-2 rounded-md' onClick={handleInputFocus}>Increment Ref</button>
     </div>
-
-
   )
 }
 
