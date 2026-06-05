0175844d5a01074d2bebb1db653dfcd10cde0cac
Ahmed Tiger <ahmeddhaqan11@gmail.com>
2026-06-05 11:15:21 +0300

    custome hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
 import React, { useState, useMemo } from 'react'
-const expensiveCalculation = (num) => {
-  console.log("Calculating...");
-  for(let i=0; i<1000000000; i++){
-    num = num+1;
-  }
-  return num;
-}
-function App() {
-  const [count, setCount] = useState(0);
-  const [todo, setTodo] = useState([]);
-const calculatedValue = expensiveCalculation(count);
-  const handleCounter=()=>{
-    setCount(count + 1);
-  }
+import useCounter from './hooks/useCounter';
 
-  const handleTodo=()=>{
-    setTodo([...todo, {id: todo.length + 1, text: "Todo " + (todo.length + 1)}]);
-  }
+function App() {
+  const [count, increment, decrement, reset ] = useCounter(0);
+
   return (
     <div>
-
-      <h2 className='text-2xl font-bold mb-4'>My Todos</h2>
-      <ul className='list-disc list-inside'>
-        {todo.map((todo)=>(
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
-      <button className='bg-blue-500 text-white p-2 rounded-md m-2' onClick={handleTodo}>Add Todo</button>
-      <hr />
       <h2 className='text-2xl font-bold mb-4'>My Counter</h2>
       <p className='text-lg mb-4'>Count: {count}</p>
-      <p className='text-lg mb-4'>Calculated Value: {calculatedValue}</p>
-      <button className='bg-blue-500 text-white p-2 rounded-md m-2' onClick={handleCounter}>Increment</button>
+
+      <button className='bg-blue-500 text-white p-2 rounded-md m-2' onClick={increment}>Increment</button>
+      <button className='bg-red-500 text-white p-2 rounded-md m-2' onClick={decrement}>Decrement</button>
+      <button className='bg-green-500 text-white p-2 rounded-md m-2' onClick={reset}>Reset</button>
     </div>
   )
 }
